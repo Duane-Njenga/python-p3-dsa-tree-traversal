b47673f9cc86beeebebfdfc39cf99bea8ebf8f03
Dwayne <dune.njenga@student.moringaschool.com>
2025-05-23 11:09:43 +0300

    Completed Lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+        stack = [self.root]
+
+        while stack:
+            node = stack.pop()
+            if node['id'] == id:
+                return node
+            stack.extend(reversed(node.get('children', [])))
+
+        return None
